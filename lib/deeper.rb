1bfdcec7fd10722fcc667cd5556a62b55a6bbd9b
james <james@derailed.(none)>
2011-07-05 12:34:02 -0400

    Added a few methods:   methods_with_paramaters (returns hash of arrays)   methods_without_paramters (returns array)
    Added private methods:
      self.extend_object
      self.included
    
      Both above methods add a method to the calling object to define the method 'deeper' which simply returns a Deeper object.
    
    * Self.included still needs testng, but I wanted to get something pushed up.

--- lib/deeper.rb
+++ lib/deeper.rb
@@ -1,14 +1,51 @@
 module Deeper
-  
-  # return array including class name and namespace
-  def arrayed_class
-    self.class.to_s.split("::")
+    class Deeper
+      def initialize(inspecter)
+        @inspecter=inspecter
+        # return array including class name and namespace
+      end
+      def class_array
+        @inspecter.class.to_s.split("::")
+      end
+      # return array of method parameters
+      def parameters(method_name = "()")
+        @inspecter.method(method_name.to_sym).parameters
+      end
+      def methods
+        return_hash = Hash.new
+        @inspecter.methods.each{|meth|
+          next if meth == 'deeper'.to_sym
+          return_hash[meth.to_sym] = parameters(meth)
+          return_hash[meth.to_sym] = nil if return_hash[meth.to_sym].empty?
+        }
+        return return_hash
+      end
+      def methods_with_parameters
+        return_hash = Hash.new
+        methods.each{|key,params|
+          return_hash[key] = params unless params.nil?
+        }
+        return return_hash
+      end
+      def methods_without_parameters
+        return_array = Array.new
+        methods.each {|key,params|
+          return_array << key
+        }
+        return return_array
+      end
+    end
+  private
+  def self.extend_object(mod)
+    mod.instance_variable_set("@deeper".to_sym, Deeper.new(mod))
+    mod.define_singleton_method(:deeper) { return @deeper }
+    super
   end
-  
-  # return array of method parameters
-  def method_parameters(method_name)
-    self.method(method_name.to_sym).parameters
+  def self.included(mod)
+    mod.send :define_method, :deeper do
+      @deeper ||= Deeper.new(self)
+      return @deeper
+    end
+
   end
-
-
 end
