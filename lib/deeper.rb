5a3cb632f09ea07d44e53cc08cd289e65edabaf6
Ian Morgan <ian@ruby-code.com>
2011-07-07 14:29:12 -0400

    commented new methods, added basic usage documentation

--- lib/deeper.rb
+++ lib/deeper.rb
@@ -1,16 +1,22 @@
 module Deeper
+  
     class Deeper
+
       def initialize(inspecter)
         @inspecter=inspecter
-        # return array including class name and namespace
       end
+      
+      # return array including class name and namespace  
       def class_array
         @inspecter.class.to_s.split("::")
       end
+  
       # return array of method parameters
       def parameters(method_name = "()")
         @inspecter.method(method_name.to_sym).parameters
       end
+  
+      # return Array of Hashes in the format of METHOD => PARAMETERS
       def methods
         return_hash = Hash.new
         @inspecter.methods.each{|meth|
@@ -20,6 +26,8 @@ module Deeper
         }
         return return_hash
       end
+      
+      # return Array of available  Hashes in the format of METHOD => PARAMETERS
       def methods_with_parameters
         return_hash = Hash.new
         methods.each{|key,params|
@@ -27,6 +35,8 @@ module Deeper
         }
         return return_hash
       end
+      
+      # return Array of available Methods that take no arguements 
       def methods_without_parameters
         return_array = Array.new
         methods.each {|key,params|
@@ -35,17 +45,18 @@ module Deeper
         return return_array
       end
     end
-  private
-  def self.extend_object(mod)
+    
+  private  
+  def self.extend_object(mod) # :nodoc:
     mod.instance_variable_set("@deeper".to_sym, Deeper.new(mod))
     mod.define_singleton_method(:deeper) { return @deeper }
     super
   end
-  def self.included(mod)
+  def self.included(mod) # :nodoc:
     mod.send :define_method, :deeper do
       @deeper ||= Deeper.new(self)
       return @deeper
-    end
+  end
 
   end
 end
